f07c2ca9cce8c40bd905b2c6103651cc9ded7fdc
Vivian Kesse <vivian@condor2363.startdedicated.com>
2024-01-08 09:35:32 -0600

    erro de falta de gpu

---
+++ files.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+from huggingface_hub import hf_hub_download
+
+hf_hub_download(repo_id="DAMO-NLP-SG/Video-LLaMA-Series", filename="pretrain_vicuna7b_audiobranch.pth")
+hf_hub_download(repo_id="DAMO-NLP-SG/Video-LLaMA-Series", filename="pretrain_vicuna7b-v2.pth")
+
+hf_hub_download(repo_id="DAMO-NLP-SG/Video-LLaMA-2-7B-Pretrained", filename="llama-2-7b-chat-hf/.gitattributes")
+hf_hub_download(repo_id="DAMO-NLP-SG/Video-LLaMA-2-7B-Pretrained", filename="llama-2-7b-chat-hf/README.md")
+hf_hub_download(repo_id="DAMO-NLP-SG/Video-LLaMA-2-7B-Pretrained", filename="llama-2-7b-chat-hf/config.json")
+hf_hub_download(repo_id="DAMO-NLP-SG/Video-LLaMA-2-7B-Pretrained", filename="llama-2-7b-chat-hf/generation_config.json")
+hf_hub_download(repo_id="DAMO-NLP-SG/Video-LLaMA-2-7B-Pretrained", filename="llama-2-7b-chat-hf/pytorch_model-00001-of-00002.bin")
+hf_hub_download(repo_id="DAMO-NLP-SG/Video-LLaMA-2-7B-Pretrained", filename="llama-2-7b-chat-hf/pytorch_model-00002-of-00002.bin")
+hf_hub_download(repo_id="DAMO-NLP-SG/Video-LLaMA-2-7B-Pretrained", filename="llama-2-7b-chat-hf/pytorch_model.bin.index.json")
+hf_hub_download(repo_id="DAMO-NLP-SG/Video-LLaMA-2-7B-Pretrained", filename="llama-2-7b-chat-hf/special_tokens_map.json")
+hf_hub_download(repo_id="DAMO-NLP-SG/Video-LLaMA-2-7B-Pretrained", filename="llama-2-7b-chat-hf/tokenizer.json")
+hf_hub_download(repo_id="DAMO-NLP-SG/Video-LLaMA-2-7B-Pretrained", filename="llama-2-7b-chat-hf/tokenizer.model")
+hf_hub_download(repo_id="DAMO-NLP-SG/Video-LLaMA-2-7B-Pretrained", filename="llama-2-7b-chat-hf/tokenizer_config.json")
+
+hf_hub_download(repo_id="DAMO-NLP-SG/Video-LLaMA-2-7B-Pretrained", filename="imagebind_huge.pth")
+hf_hub_download(repo_id="DAMO-NLP-SG/Video-LLaMA-2-7B-Pretrained", filename="AL_LLaMA_2_7B_Pretrained.pth")
+hf_hub_download(repo_id="DAMO-NLP-SG/Video-LLaMA-2-7B-Pretrained", filename="VL_LLaMA_2_7B_Pretrained.pth")
+hf_hub_download(repo_id="DAMO-NLP-SG/Video-LLaMA-2-7B-Pretrained", filename=".gitattributes")
